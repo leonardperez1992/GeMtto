8ee405e5d3cd3c760f61a2110a38188a0de0c544
leonardperez1992 <leonardperez1992@gmail.com>
2024-01-13 13:35:02 -0500

    stylos

--- src/Router.js
+++ src/Router.js
@@ -4,10 +4,7 @@ import { useDispatch } from 'react-redux';
 import { saveUser } from './redux/actions/user.action';
 import Login from './views/Login';
 import Registro from './views/Registro';
-import Informes from './views/Informes';
-import Reservar from './views/Reservar';
 import Navbar from './components/Navbar';
-import EditEmp from './views/EditEmp';
 import Inventario from './views/Inventario';
 import CreateInventary from './views/CreateInventary';
 import ReporteService from './views/ReporteService';
@@ -39,11 +36,8 @@ const RouterContainer = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route path="/editempleados" element={<EditEmp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
-          <Route path="/Informes" element={<Informes />} />
-          <Route path="/reservar" element={<Reservar />} />
           <Route path="/inventarioua" element={<Inventario />} />
           <Route path="/createinventary" element={<CreateInventary />} />
           <Route path="/reporteService" element={<ReporteService />} />

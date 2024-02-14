5bd1f3618c4086a224cc5a7b8637dc6fdaeb0f59
leonardperez1992 <leonardperez1992@gmail.com>
2024-02-14 12:27:12 -0500

    update inv y navbar

--- src/views/Inventario.js
+++ src/views/Inventario.js
@@ -38,89 +38,82 @@ function Inventario() {
   }
   return (
     <div className="contenedor">
-      <main>
-        <section>
-          <div>
-            <div>
-              <h3>Inventario General</h3>
-            </div>
-            <div>
-              <div
-                style={{
-                  width: '20%',
-                  margin: 10,
-                }}
-              >
-                <h4>Buscar:</h4>
-                <input
-                  style={{
-                    width: '100%',
-                    borderWidth: 1,
-                    margin: 5,
-                    borderRadius: 10,
-                    borderStyle: 'solid',
-                    height: 43,
-                  }}
-                  value={buscar}
-                  type="text"
-                  placeholder="Digite la serie"
-                  onChange={handleSave}
-                />
-              </div>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>EQUIPO</th>
-                    <th>MARCA</th>
-                    <th>MODELO</th>
-                    <th>SERIE</th>
-                    <th>INSTITUCION</th>
-                    <th>SERVICIO</th>
-                    <th>UBICACIÓN</th>
-                    <th>REG. INVIMA</th>
-                    <th>RIESGO</th>
-                    <th>RESPONSABLE</th>
-                    <th>ACCION</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {inventarios.map(function (item) {
-                    return (
-                      <tr>
-                        <td>{item?.equipo}</td>
-                        <td>{item?.marca}</td>
-                        <td>{item?.modelo}</td>
-                        <td>{item?.serie}</td>
-                        <td>{item?.institucion}</td>
-                        <td>{item?.servicio}</td>
-                        <td>{item?.ubicacion}</td>
-                        <td>{item?.registro_invima}</td>
-                        <td>{item?.riesgo}</td>
-                        <td>{item?.responsable}</td>
-                        <td>
-                          <Link
-                            to={`/reporteService?id=${item._id}`}
-                            className="nav-link"
-                          >
-                            Reporte
-                          </Link>
-                          <br></br>
-                          <Link
-                            to={`/hojadevida?id=${item._id}&modelo=${item.modelo}&serie=${item.serie}&institucion=${item.institucion}`}
-                            className="nav-link"
-                          >
-                            Hoja de Vida
-                          </Link>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+      <div>
+        <div>
+          <div
+            style={{
+              width: '20%',
+              margin: 10,
+            }}
+          >
+            <h4>Buscar:</h4>
+            <input
+              style={{
+                width: '100%',
+                borderWidth: 1,
+                margin: 5,
+                borderRadius: 10,
+                borderStyle: 'solid',
+                height: 43,
+              }}
+              value={buscar}
+              type="text"
+              placeholder="Digite la serie"
+              onChange={handleSave}
+            />
           </div>
-        </section>
-      </main>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>EQUIPO</th>
+                <th>MARCA</th>
+                <th>MODELO</th>
+                <th>SERIE</th>
+                <th>INSTITUCION</th>
+                <th>SERVICIO</th>
+                <th>UBICACIÓN</th>
+                <th>REG. INVIMA</th>
+                <th>RIESGO</th>
+                <th>RESPONSABLE</th>
+                <th>ACCION</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inventarios.map(function (item) {
+                return (
+                  <tr>
+                    <td>{item?.equipo}</td>
+                    <td>{item?.marca}</td>
+                    <td>{item?.modelo}</td>
+                    <td>{item?.serie}</td>
+                    <td>{item?.institucion}</td>
+                    <td>{item?.servicio}</td>
+                    <td>{item?.ubicacion}</td>
+                    <td>{item?.registro_invima}</td>
+                    <td>{item?.riesgo}</td>
+                    <td>{item?.responsable}</td>
+                    <td>
+                      <Link
+                        to={`/reporteService?id=${item._id}`}
+                        className="nav-link"
+                      >
+                        Reporte
+                      </Link>
+                      <br></br>
+                      <Link
+                        to={`/hojadevida?id=${item._id}&modelo=${item.modelo}&serie=${item.serie}&institucion=${item.institucion}`}
+                        className="nav-link"
+                      >
+                        Hoja de Vida
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
